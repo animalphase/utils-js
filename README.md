5f13c0deef8c4428ffeb3d15d065d93460b206c8
Audrey Moon <audrey@loveme.computer>
2018-02-04 14:23:32 -0600

    added getPropValue and imageExists

--- README.md
+++ README.md
@@ -61,6 +61,25 @@ Takes arguments of `propertyToMatch`, `desiredValue`, and `arrayToSearch`.
 Returns `null` if none are found.
 
 
+
+### getPropValue
+
+search within nested properties of an object, stops searching when hitting a null property
+e.g. for this.soundCloudAudio._track.user.avatar_url,
+let avatarUrl = getPropValue( ['_track', 'user', 'avatar_url'], this.soundCloudAudio ) // assigned null if property doesn't exist
+@param  {Array} pathToProp   the path to a nested prop we hope to find, each step listed as an array value (string for keys, number for indexes)
+@param  {Object} objectToScan the object to look through for all available properties based on the path above
+@return {varies}              returns null, or the value found at the requested property
+
+
+
+### imageExists
+
+check if image exists at url
+@param  {string} image_url the url of image to test
+@return {bool}           does the image exist or not
+
+
 ---
 
 
--- utils.js
+++ utils.js
@@ -1,11 +1,10 @@
 const utils = {
-  // math functions
   clamp: (value, min, max) => {
     return value <= min ? min : value >= max ? max : value;
   },
   sleep: milliseconds => {
     return new Promise(resolve => setTimeout(resolve, milliseconds));
-  },  
+  },
   indexesToIds: array => {
     var lookup = {};
     for (var i = 0, length = array.length; i < length; i++) {
@@ -29,6 +28,31 @@ const utils = {
       console.log('⚠️ utils.indexOfObjectWithPropertyValue found nothing!!');
       return null;
     }
+  },
+  /**
+   * search within nested properties of an object, stops searching when hitting a null property
+   * e.g. for this.soundCloudAudio._track.user.avatar_url,
+   * let avatarUrl = getPropValue( ['_track', 'user', 'avatar_url'], this.soundCloudAudio ) // assigned null if property doesn't exist
+   * @param  {Array} pathToProp   the path to a nested prop we hope to find, each step listed as an array value (string for keys, number for indexes)
+   * @param  {Object} objectToScan the object to look through for all available properties based on the path above
+   * @return {varies}              returns null, or the value found at the requested property
+   */
+  getPropValue: (pathToProp, objectToScan) => {
+    return pathToProp.reduce(
+      (step, otherStep) => (step && step[otherStep] ? step[otherStep] : null),
+      objectToScan
+    )
+  },
+  /**
+   * check if image exists at url
+   * @param  {string} image_url the url of image to test
+   * @return {bool}           does the image exist or not
+   */
+  imageExists: image_url => {
+    var http = new XMLHttpRequest()
+    http.open('HEAD', image_url, false)
+    http.send()
+    return http.status != 404
   }
 };
 
